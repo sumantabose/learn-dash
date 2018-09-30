d6e68af5f6bc1255729588f3150da8c7e0a400e8
BlockchainNTU <BlockchainNTU@Gmail.Com>
2018-10-01 01:20:24 +0800

    added 7-css-js/

---
+++ 7-css-js/app.py
@@ -0,0 +1,28 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+    html.Div(
+        className="app-header",
+        children=[
+            html.Div('Plotly Dash', className="app-header--title")
+        ]
+    ),
+    html.Div(
+        children=html.Div([
+            html.H5('Overview'),
+            html.Div('''
+                This is an example of a simple Dash app with
+                local, customized CSS.
+            ''')
+        ])
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
