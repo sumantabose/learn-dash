d18181bfdeb59ba1dbcfda9de874805603914850
BlockchainNTU <BlockchainNTU@Gmail.Com>
2018-10-01 02:30:26 +0800

    added local-css-js/ and custom-index/

---
+++ 7-css-js/custom-index/app.py
@@ -0,0 +1,32 @@
+import dash
+import dash_html_components as html
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.index_string = '''
+<!DOCTYPE html>
+<html>
+    <head>
+        {%metas%}
+        <title>My Custom Title</title>
+        {%favicon%}
+        {%css%}
+    </head>
+    <body>
+        <div>My Custom header</div>
+        {%app_entry%}
+        <footer>
+            {%config%}
+            {%scripts%}
+        </footer>
+        <div>My Custom footer</div>
+    </body>
+</html>
+'''
+
+app.layout = html.Div('Simple Dash App')
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
